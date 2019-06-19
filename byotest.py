f8cb683a60f613fc4dd1492e415b124f034fa2d4
Stephen Byrne <s.byrne6@gmail.com>
2019-06-19 10:16:50 +0000

    refactoring

--- byotest.py
+++ byotest.py
@@ -1,10 +1,3 @@
-# def number_of_evens(numbers):
-#     return 0
-
-
-# def test_are_equal(actual, expected):
-#     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
-    
 # challenges
     
 def test_not_equal(a, b):
@@ -14,13 +7,10 @@ def test_is_in(collection, item):
     assert item in collection, "{0} does not contain {1}".format(collection, item)
     
 def test_not_in(collection, item):
-    assert item not in collection, "Did not expect {0} to contain {1} but does".format(collection, item)
+    assert item not in collection, "{0} is in {1}".format(item, collection)
     
-def test_between(a, b, c):
-    assert a >= b and a <=c, "Expected True but got False"
-    
-    
-# test_are_equal(number_of_evens([1,2,3,4,5]), 2)
+def test_between(upper_limit, lower_limit, actual):
+    assert lower_limit <= actual <= upper_limit, "{0} is not between {1} and {2}".format(actual, lower_limit, upper_limit)
 
 test_not_equal(5,4)
 
@@ -28,5 +18,5 @@ test_is_in([5,7,89], 5)
 
 test_not_in([3,9,601,56,12], 4)
 
-test_between(7, 3, 10)
+test_between(10, 3, 7)
 print("all tests passed!")
