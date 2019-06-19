e97341b44d7c0e03e64297786f9939e0a20037f0
Stephen Byrne <s.byrne6@gmail.com>
2019-06-19 10:11:41 +0000

    tests added

--- byotest.py
+++ byotest.py
@@ -1,9 +1,9 @@
-def number_of_evens(numbers):
-    return 0
+# def number_of_evens(numbers):
+#     return 0
 
 
-def test_are_equal(actual, expected):
-    assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
+# def test_are_equal(actual, expected):
+#     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
     
 # challenges
     
@@ -20,4 +20,13 @@ def test_between(a, b, c):
     assert a >= b and a <=c, "Expected True but got False"
     
     
-test_are_equal(number_of_evens([1,2,3,4,5]), 2)
+# test_are_equal(number_of_evens([1,2,3,4,5]), 2)
+
+test_not_equal(5,4)
+
+test_is_in([5,7,89], 5)
+
+test_not_in([3,9,601,56,12], 4)
+
+test_between(7, 3, 10)
+print("all tests passed!")
